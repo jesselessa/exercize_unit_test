7e1c2cbcaf8592abc3a4d005d62666093963491a
jess <jessica.elessa@gmail.com>
2022-04-04 16:11:41 +0200

    test isOddNumber passed

--- test.js
+++ test.js
@@ -1,8 +1,15 @@
 //-------------- UNIT TESTS ----------------//
 
+// * Even number
 const isEvenNumber = require("./utils/isEvenNumber");
 
 test("if number is even", () => {
   const result = isEvenNumber(18);
   expect(result).toBe(true);
+});
+
+// * Odd number
+test("if number is Odd", () => {
+  const result = isEvenNumber(15);
+  expect(result).toBe(false);
 });
