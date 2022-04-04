6fb196627ddddcfd47d3b8ff6fabddcaf7ff1040
jess <jessica.elessa@gmail.com>
2022-04-04 16:25:37 +0200

    tests isEven bonuses passed

--- test.js
+++ test.js
@@ -1,15 +1,26 @@
 //-------------- UNIT TESTS ----------------//
 
-// * Even number
 const isEvenNumber = require("./utils/isEvenNumber");
 
+//*  IS EVEN
 test("if number is even", () => {
   const result = isEvenNumber(18);
   expect(result).toBe(true);
 });
 
-// * Odd number
+// *! Odd number
 test("if number is Odd", () => {
   const result = isEvenNumber(15);
   expect(result).toBe(false);
+});
+
+//* IS EVEN BONUS
+test("if it's a string", () => {
+  const result = isEvenNumber("string");
+  expect(result).toBe(false);
+});
+
+test("if it's a string of numbers", () => {
+  const result = isEvenNumber("20");
+  expect(result).toBe(true);
 });
